4c9f712fad5db99099ee59ea0c8bb039e090a682
nanjizal <nitsuj@nanjizal.net>
2017-06-06 13:27:18 +0100

    Update Settings.hx

--- xj/Settings.hx
+++ xj/Settings.hx
@@ -2,14 +2,30 @@ package xj;
 
 class Settings {
     inline public static var attributeSymbol: String = '@';
+    
     #if doubleQuotes
         inline public static var quoteSymbol: String = '"';
     #else 
         inline public static var quoteSymbol: String = '"'; 
     #end
+    
     #if windowLineEnd
         inline public static var lineEndSymbol: String = '\r\n';
     #else 
         inline public static var lineEndSymbol: String = '\n';
+    #end
+    
+    #if indent0
+        inline public static var indentString: String = '';
+    #elseif indent1
+        inline public static var indentString: String = ' ';
+    #elseif indent2
+        inline public static var indentString: String = '  ';
+    #elseif indent3
+        inline public static var indentString: String = '   ';
+    #elseif indent4
+        inline public static var indentString: String = '    ';
+    #else  
+        inline public static var indentString: String = '   ';
     #end
 }
